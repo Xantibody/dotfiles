75c20d6354d4ac5ca6d7eda2fdeef244ca9e4159
r-aizawa <zeku.bushinryu38@gmail.com>
2025-02-01 01:13:08 +0900

    fxi quickfix

--- nvim/lua/plugins/render-markdown.lua
+++ nvim/lua/plugins/render-markdown.lua
@@ -4,9 +4,9 @@ return {
     "markdown",
   },
   keys = {
-    { "<C-m>", "<cmd>RenderMarkdown toggle<cr>", mode = "n", desc = "Toggle Render Markdown" }
+    { "<leader>rm", "<CMD>RenderMarkdown toggle<CR>", mode = "n", desc = "Toggle Render Markdown" }
   },
   opts = {},
-  dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
-  -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
+  --  dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
+  dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
 }
