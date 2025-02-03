3b4ce560edeccb13c80f298df2a3d1de61caebe9
r-aizawa <zeku.bushinryu38@gmail.com>
2025-02-04 08:46:25 +0900

    typo

--- fish/config.fish
+++ fish/config.fish
@@ -1,7 +1,7 @@
 fish_vi_key_bindings
 
-function dpnvim
-    set -gx NVIM_APPNAME "darkpawered"
+function dpvim
+    set -gx NVIM_APPNAME "darkpowered"
     nvim
     set -e NVIM_APPNAME  # 終了後に環境変数を削除
 end
