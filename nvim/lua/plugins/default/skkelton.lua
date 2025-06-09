3bddce2463cfb6df048690948186da43e9cb0852
Xantibody <zeku.bushinryu38@gmail.com>
2025-06-09 10:19:36 +0900

    display skkleton status for lualine

--- nvim/lua/plugins/default/skkelton.lua
+++ nvim/lua/plugins/default/skkelton.lua
@@ -1,12 +1,26 @@
 return {
 	"vim-skk/skkeleton",
 	event = "VeryLazy",
-	dependencies = { "vim-denops/denops.vim" },
+	dependencies = {
+		"vim-denops/denops.vim",
+	},
 	config = function()
 		vim.fn["skkeleton#config"]({
 			eggLikeNewline = true,
 			registerConvertResult = true,
 			globalDictionaries = { "~/.skk/SKK-JISYO.L" },
+		})
+
+		-- lualine setting
+		vim.api.nvim_create_autocmd({ "User" }, {
+			desc = "Refresh statusline to show the mode for Skkeleton",
+			pattern = "skkeleton-mode-changed",
+			group = vim.api.nvim_create_augroup("lualine-skkeleton-mode", {}),
+			callback = function()
+				-- 	-- tabline に表示している場合。↓を詳しくは参照。
+				-- 	-- https://github.com/nvim-lualine/lualine.nvim/blob/05d78e9fd0cdfb4545974a5aa14b1be95a86e9c9/doc/lualine.txt#L975
+				require("lualine").refresh({ place = { "statusline" } })
+			end,
 		})
 
 		vim.keymap.set("i", "<C-j>", "<Plug>(skkeleton-enable)")
--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,3 +1,25 @@
+local function skkeleton()
+	local ok, mode = pcall(vim.fn["skkeleton#mode"])
+	if not ok then
+		return ""
+	end
+
+	if mode == "hira" then
+		return "ひら"
+	elseif mode == "kata" then
+		return "カタ"
+	elseif mode == "hankata" then
+		return "半カナ"
+	elseif mode == "zenkaku" then
+		return "全英"
+	elseif mode == "abbrev" then
+		return "abbr"
+	else
+		return "英数"
+	end
+	return ""
+end
+
 return {
 	"nvim-lualine/lualine.nvim",
 	dependencies = {
@@ -8,7 +30,12 @@ return {
 			theme = "everforest",
 		},
 		sections = {
-			lualine_x = { "overseer" },
+			lualine_x = { skkeleton },
 		},
+	},
+	extensions = {
+		"fzf",
+		"oil",
+		"quickfix",
 	},
 }
