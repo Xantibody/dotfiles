382004b122624687fdd54696076daa39a5274189
Xantibody <zeku.bushinryu38@gmail.com>
2025-06-19 09:42:19 +0900

    fix(hyprpanel): fix clock and icon setting for hyprpanel

--- home-manager/home/packages.nix
+++ home-manager/home/packages.nix
@@ -4,7 +4,7 @@
 }:
 with pkgs;
 [
-  #  pympress
+  pympress
   google-chrome
   bat
   cliphist
@@ -19,30 +19,5 @@ with pkgs;
   nix-prefetch-github
   ripgrep
   zellij
-  # (buildGoModule {
-  #       pname = "iccheck";
-  #       version = "0.9.0";
-  #       src = fetchFromGitHub {
-  #         owner = "salab";
-  #         repo = "iccheck";
-  #         rev = "v0.9.0";
-  #         sha256 = "sha256-2bD5gN/7C79njrCVoR5H2ses6pWAQHZcYj7/f2+Ui/o=";
-  #       };
-  #       vendorHash = "sha256-pqjtoshoQlz+SFpaaxN3GMaDdZ+ztiIV6w+CTrRHuaA=";
-  #       meta = with lib; {
-  #         homepage = "https://github.com/salab/iccheck";
-  #       };
-  #       doCheck = false;
-  #       subPackages = [
-  #         "."
-  #         "./cmd"
-  #         "./pkg/domain"
-  #         "./pkg/fleccs"
-  #         "./pkg/lsp"
-  #         "./pkg/ncdsearch"
-  #         "./pkg/printer"
-  #         "./pkg/search"
-  #         "./pkg/utils"
-  #       ];
-  #     })
+  iccheck
 ]
