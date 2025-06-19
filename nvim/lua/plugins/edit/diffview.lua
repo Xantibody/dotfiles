23419bd0bfa74ce8622d575127cde80905cf0397
Xantibody <zeku.bushinryu38@gmail.com>
2025-06-19 11:58:35 +0900

    fix(diffview): add diffView keymaps

--- nvim/lua/plugins/edit/diffview.lua
+++ nvim/lua/plugins/edit/diffview.lua
@@ -1,3 +1,18 @@
 return {
 	"sindrets/diffview.nvim",
+	config = function()
+		require("diffview").setup({
+			keymaps = {
+				view = {
+					{ "n", "q", "<cmd>DiffviewClose<cr>", { desc = "Close Diffview" } },
+				},
+				file_panel = {
+					{ "n", "q", "<cmd>DiffviewClose<cr>", { desc = "Close File Panel" } },
+				},
+				file_history_panel = {
+					{ "n", "q", "<cmd>DiffviewClose<cr>", { desc = "Close File History" } },
+				},
+			},
+		})
+	end,
 }
