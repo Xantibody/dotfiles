1b9eee395d16e1a4b52aad34403ad2824661059c
Xantibody <zeku.bushinryu38@gmail.com>
2025-07-03 03:40:27 +0900

    fix typst.lua

--- nvim/lua/plugins/preview/typst.lua
+++ nvim/lua/plugins/preview/typst.lua
@@ -1,10 +1,8 @@
 return {
-	{
-		"kaarmu/typst.vim",
-		ft = "typst",
-		lazy = false,
-		config = function()
-			vim.g.typst_pdf_viewer = "zathura"
-		end,
-	},
+	"kaarmu/typst.vim",
+	ft = "typst",
+	lazy = false,
+	config = function()
+		vim.g.typst_pdf_viewer = "zathura"
+	end,
 }
