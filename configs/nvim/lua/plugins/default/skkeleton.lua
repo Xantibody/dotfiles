d032d85249c67eaa370f7328aa98590128e2a1c8
Xantibody <zeku.bushinryu38@gmail.com>
2025-10-27 17:52:43 +0900

    add: skkeleton keybind

--- configs/nvim/lua/plugins/default/skkeleton.lua
+++ configs/nvim/lua/plugins/default/skkeleton.lua
@@ -13,7 +13,6 @@ return {
 			-- This forces all candidates to be shown, which blink.cmp can then display
 			showCandidatesCount = 999,
 		})
-
 		-- lualine setting
 		vim.api.nvim_create_autocmd({ "User" }, {
 			desc = "Refresh statusline to show the mode for Skkeleton",
@@ -28,5 +27,7 @@ return {
 
 		vim.keymap.set("i", "<C-j>", "<Plug>(skkeleton-enable)")
 		vim.keymap.set("c", "<C-j>", "<Plug>(skkeleton-enable)")
+		vim.fn["skkeleton#register_keymap"]("henkan", "<C-n>", "henkanForward")
+		vim.fn["skkeleton#register_keymap"]("henkan", "<C-p>", "henkanBackward")
 	end,
 }
