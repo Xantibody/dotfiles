4f0cf3a11123d659e4cdf181adb5af325995f863
Xantibody <zeku.bushinryu38@gmail.com>
2025-10-28 16:07:22 +0900

    fix: key-menuの設定をコンパクトに

--- configs/nvim/lua/plugins/default/key-menu.lua
+++ configs/nvim/lua/plugins/default/key-menu.lua
@@ -1,43 +1,17 @@
 return {
 	"emmanueltouzery/key-menu.nvim",
-	event = "VeryLazy", -- 起動を軽く
-	opts = {}, -- そのままでOK（必要なら設定を書く）
+	event = "VeryLazy",
 	init = function()
 		-- 複数キーの判定待ち
 		vim.o.timeout = true
 		vim.o.timeoutlen = 300
 	end,
 	config = function()
-		-- <Space> を押したらポップアップが出る“前置きキー”として登録
-		require("key-menu").set("n", "<Space>")
+		-- 表示設定
+		require("key-menu").set("n", "<Leader>")
+		require("key-menu").set("n", "g")
 
-		-- Git サブグループの見出し
-		require("key-menu").set("n", "<Space>g", { desc = "Git" })
-
-		-- バッファ限定グループ（例）
-		require("key-menu").set("n", "<Space>s", { desc = "Say something", buffer = true })
+		-- TODO:グループ設定を考える
+		require("key-menu").set("n", "<Leader>g", { desc = "Git" })
 	end,
-	-- ふだんのマップは lazy の keys で宣言するとスッキリ
-	keys = {
-		-- グローバル系
-		{ "<Space>w", "<Cmd>w<CR>", desc = "Save", mode = "n" },
-		{ "<Space>q", "<Cmd>q<CR>", desc = "Quit", mode = "n" },
-
-		-- Lua 関数も可
-		{
-			"<Space>k",
-			function()
-				vim.api.nvim_buf_set_lines(0, 0, -1, false, {})
-			end,
-			desc = "Erase all",
-			mode = "n",
-		},
-
-		-- Git グループの中身
-		{ "<Space>gs", "<Cmd>Git status<CR>", desc = "Status", mode = "n" },
-		{ "<Space>gc", "<Cmd>Git commit<CR>", desc = "Commit", mode = "n" },
-
-		-- メニューに出さない隠しマップの例（key-menu では表示されない）
-		{ "<leader>1", "<Cmd>BufferLineGoToBuffer 1<CR>", desc = "HIDDEN", mode = "n" },
-	},
 }
