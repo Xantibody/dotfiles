b494b0666c76bb594b070d8bdde51839cacbe302
Xantibody <zeku.bushinryu38@gmail.com>
2025-10-28 16:09:32 +0900

    fix: neoscrollの無駄なkeymapを削除

--- configs/nvim/lua/plugins/display/neoscroll.lua
+++ configs/nvim/lua/plugins/display/neoscroll.lua
@@ -1,4 +1,13 @@
 return {
 	"karb94/neoscroll.nvim",
 	opts = {},
+	config = function()
+		require("neoscroll").setup({
+			-- mappingを減らす
+			mappings = {
+				"<C-u>",
+				"<C-d>",
+			},
+		})
+	end,
 }
