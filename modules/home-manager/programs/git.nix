f7c5e8302f6e062fcef58672e9e726f8de6b37f9
Xantibody <zeku.bushinryu38@gmail.com>
2025-12-20 01:12:42 +0900

    fix: add sihning settnig for git

--- modules/home-manager/programs/git.nix
+++ modules/home-manager/programs/git.nix
@@ -1,10 +1,15 @@
 {
   enable = true;
+  signing = {
+    format = "ssh";
+    signByDefault = true;
+    key = "~/.ssh/id_ed25519.pub";
+  };
   settings = {
     user = {
       name = "Xantibody";
       email = "zeku.bushinryu38@gmail.com";
-      core.editor = "nvim";
     };
+    core.editor = "nvim";
   };
 }
