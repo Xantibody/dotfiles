b17b969b9ae4b3cc292b40d0d5c440b3c3d4797d
Xantibody <zeku.bushinryu38@gmail.com>
2026-01-14 23:41:29 +0900

    fix: enable treesitter syntax highlighting for all filetypes
    Treesitter grammars were provided by Nix but highlighting was not
    enabled. Use vim.treesitter.start() via FileType autocmd.

--- modules/home-manager/programs/nixcats/lua/config/plugins/core/treesitter.lua
+++ modules/home-manager/programs/nixcats/lua/config/plugins/core/treesitter.lua
@@ -1,2 +1,9 @@
 -- Treesitter configuration (grammars are provided by Nix)
 vim.treesitter.language.register("bash", "zsh")
+
+-- Enable treesitter highlighting for all filetypes
+vim.api.nvim_create_autocmd("FileType", {
+	callback = function()
+		pcall(vim.treesitter.start)
+	end,
+})
