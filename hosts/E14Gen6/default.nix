2653e40192b1e7185d92c74fc1b2a60c89fa8cbe
Xantibody <zeku.bushinryu38@gmail.com>
2026-02-17 01:33:32 +0900

    refactor(nixos): use nixpkgs.overlays instead of manual pkgs
    Move overlay configuration from manual pkgs import to nixpkgs.overlays
    module option. This ensures overlays are properly applied to the
    NixOS system pkgs used by home-manager with useGlobalPkgs = true.

--- hosts/E14Gen6/default.nix
+++ hosts/E14Gen6/default.nix
@@ -6,8 +6,6 @@ let
     sops-nix
     home-manager
     self
-    firefox-addons
-    zen-browser
     ;
 
   commonOverlays = import ../overlays.nix { inherit inputs; };
@@ -16,45 +14,43 @@ let
   username = "raizawa";
   system = "x86_64-linux";
   homeDirectory = "/home/${username}";
-  pkgs = import nixpkgs {
-    inherit system;
-    overlays =
-      commonOverlays
-      ++ [
-        firefox-addons.overlays.default
-      ]
-      ++ (import ../../overlays);
-    config.allowUnfree = true;
-  };
-
 in
 nixpkgs.lib.nixosSystem {
   inherit system;
-  specialArgs = { inherit username xremap nixpkgs; };
+  specialArgs = { inherit username xremap nixpkgs inputs; };
 
   modules = [
+    {
+      nixpkgs.overlays =
+        commonOverlays
+        ++ [ inputs.firefox-addons.overlays.default ]
+        ++ (import ../../overlays);
+    }
     ../../modules/nixos
     ../../modules/nixos/hardware-configuration.nix
     sops-nix.nixosModules.sops
     home-manager.nixosModules.home-manager
-    {
-      home-manager = {
-        useGlobalPkgs = true;
-        backupFileExtension = "backup";
-        extraSpecialArgs = { inherit inputs; };
-        sharedModules = commonHomeModules ++ [
-          sops-nix.homeManagerModules.sops
-        ];
-        users."${username}" = import ../../modules/home-manager {
-          inherit
-            pkgs
-            inputs
-            homeDirectory
-            username
-            self
-            ;
+    (
+      { pkgs, ... }:
+      {
+        home-manager = {
+          useGlobalPkgs = true;
+          backupFileExtension = "backup";
+          extraSpecialArgs = { inherit inputs; };
+          sharedModules = commonHomeModules ++ [
+            sops-nix.homeManagerModules.sops
+          ];
+          users."${username}" = import ../../modules/home-manager {
+            inherit
+              pkgs
+              inputs
+              homeDirectory
+              username
+              self
+              ;
+          };
         };
-      };
-    }
+      }
+    )
   ];
 }
