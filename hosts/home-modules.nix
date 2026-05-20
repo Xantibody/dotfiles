701ef23a5a076d1ac80a4e521f851fce6e945f1a
Xantibody <zeku.bushinryu38@gmail.com>
2026-05-20 22:03:46 +0900

    feat(home-manager): enable comma via nix-index-database
    Wrap comma with the prebuilt nix-index database so that command
    lookups work without manually maintaining the index.

--- hosts/home-modules.nix
+++ hosts/home-modules.nix
@@ -2,4 +2,6 @@
 [
   # nixCats neovim configuration
   ../modules/home-manager/programs/nixcats
+  inputs.nix-index-database.homeModules.default
+  { programs.nix-index-database.comma.enable = true; }
 ]
