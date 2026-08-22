d5e1e7cc69fbc2e4fda9109fa1f575fe745bd04a
Xantibody <zeku.bushinryu38@gmail.com>
2026-08-22 14:47:41 +0900

    feat(git): set ghq.user to the GitHub handle
    ghq get を org 名なしの引数で叩けるようにする。既に user.name が同じ
    GitHub ハンドルを持っていたため、githubUser として let で括り出し、
    両者が食い違わないようにした。

--- modules/home-manager/programs/git.nix
+++ modules/home-manager/programs/git.nix
@@ -1,3 +1,8 @@
+# git の author 名と ghq の default owner は同じ GitHub ハンドルなので
+# 1 箇所で定義する。
+let
+  githubUser = "Xantibody";
+in
 {
   enable = true;
   signing = {
@@ -7,17 +12,20 @@
   };
   settings = {
     user = {
-      name = "Xantibody";
+      name = githubUser;
       email = "zeku.bushinryu38@gmail.com";
     };
     core.editor = "nvim";
     init.defaultBranch = "main";
     push.useForceIfIncludes = true;
-    # 最後の root が primary となり、新規 clone 先になる。
-    # 会社 org は host 側の ghq.<url>.root で work へ振り分ける。
-    ghq.root = [
-      "~/Repository/work"
-      "~/Repository/private"
-    ];
+    ghq = {
+      # 最後の root が primary となり、新規 clone 先になる。
+      # 会社 org は host 側の ghq.<url>.root で work へ振り分ける。
+      root = [
+        "~/Repository/work"
+        "~/Repository/private"
+      ];
+      user = githubUser;
+    };
   };
 }
