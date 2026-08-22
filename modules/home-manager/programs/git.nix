f6ed31b9cd4e8ba630fa3e523882bd9ba40e7157
Xantibody <zeku.bushinryu38@gmail.com>
2026-08-22 13:46:40 +0900

    feat(git): default new repositories to the main branch
    ghq create は内部で git init を呼ぶだけなので、init.defaultBranch が未設定だと
    Git 既定の master で初期化されてしまう。GitHub 側の既定は main であり、作成の
    たびに git branch -m する手間が発生していた。

--- modules/home-manager/programs/git.nix
+++ modules/home-manager/programs/git.nix
@@ -11,6 +11,7 @@
       email = "zeku.bushinryu38@gmail.com";
     };
     core.editor = "nvim";
+    init.defaultBranch = "main";
     push.useForceIfIncludes = true;
     # 最後の root が primary となり、新規 clone 先になる。
     # 会社 org は host 側の ghq.<url>.root で work へ振り分ける。
